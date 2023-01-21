9eaa3a26ff3004ab4af88faee5391174951d5497
AdilsonCp <adilsonmedeiros70@gmail.com>
2023-01-21 12:36:46 -0300

    limpar tela

---
+++ limpa tela.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+import time
+while True:
+    num = int(input("Digite um número: "))
+    print(f"O número digitado foi {num}")
+    if num == 999:
+        break
+    time.sleep(2)
+    os.system("cls") or None
+print("Fim")
+
+
+# In[ ]:
+
+
+
+
